d70fa7626dda9762679424116aedebb9dcb9a1de
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-01-13 16:07:34 +0100

    basic starting code

---
+++ src/scenes/Game.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+    preload() {
+        this.load.image('tile', '../assets/tile.png')
+    }
+
+    create() {
+        
+    }
+}
